09d777e7120e34fc1981f05778d02a04d03c3e30
mttette <mohammed005tahseen972@gmail.com>
2022-12-27 22:03:17 +0300

    0.0.3

--- app.py
+++ app.py
@@ -6,12 +6,25 @@ API_ID = "17156186"
 API_HASH = "c2957f899e43014b76d8df20574764c3"
 BOT_TOKEN = "5901972668:AAHj1SNZ0nTDvOEAFwtqGZtOHH2s7EFQ7Z0"
 
+admin_id = 1667320421
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH,bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("start"))
 def start(client, message):
     message.reply_text("اهلا وسهلا, ما هو اسمك الكامل(الثلاثي)؟")
+
+@app.on_message(filters.chat(admin_id) & filters.command("help"))
+def help(client, message):
+    message.reply_text("""
+    welcome admin , here is a list of the commands that u can do:
+
+    /set_welcome_txt -- to set the welcome text
+    /set_qustion_txt to set qustion text 
+    /upload_xlsx to upload the new xlsx file 
+    /dev to contact with the devolopers
+
+    """)
 
 @app.on_message(filters.text)
 def handle_message(client, message):
